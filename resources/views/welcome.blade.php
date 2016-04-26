be3f26a805f31d05114f7720d396af4667831c8f
NikaPolosina <polosina.nika@gmail.com>
2016-04-26 22:28:25 +0300

    modification welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,13 @@
             @if (Auth::guest())
                     <div class="col-md-2 col-md-push-10">
                         <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Войти</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-12 control-label register_right">E-Mail Address</label>
+                            <label class="col-md-12 control-label register_right">E-Mail</label>
 
                             <div class="col-md-12">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -29,7 +29,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-12 control-label register_right">Password</label>
+                            <label class="col-md-12 control-label register_right">Пароль</label>
 
                             <div class="col-md-12">
                                 <input type="password" class="form-control" name="password">
@@ -46,7 +46,7 @@
                             <div class="col-md-12">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input type="checkbox" name="remember"> Запомнить меня
                                     </label>
                                 </div>
                             </div>
@@ -55,7 +55,7 @@
                         <div class="form-group">
                             <div class="col-md-12">
                                 <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i>Login
+                                    <i class="fa fa-btn fa-sign-in"></i>Вход
                                 </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Забыл пароль?</a>
