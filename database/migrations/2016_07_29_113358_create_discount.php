d876a8c7f2338d4c0a4f6fab6ae9251baaf91663
Nika <polosina.nika@gmail.com>
2016-07-29 14:50:11 +0300

    create table discount

---
+++ database/migrations/2016_07_29_113358_create_discount.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discount_accumulative', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('from')->unsigned();
+            $table->integer('to')->unsigned();
+            $table->integer('percent')->unsigned();
+            $table->integer('company_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('company_id')
+                ->references('id')->on('companies')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('discount_accumulative');
+    }
+}
