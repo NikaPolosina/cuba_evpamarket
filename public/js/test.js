b4920bbb936f1b18e74a702bd95a1e8f4b625969
Nika <polosina.nika@gmail.com>
2016-09-05 18:53:25 +0300

    modal asc test

---
+++ public/js/test.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('button.tut').on('click', function () {
+        $('#modal_delete').modal();
+        console.log($('#modal_delete'));
+
+        
+    });
+    /**
+     * Show modal window with current product
+     * */
+   
+
+    });
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,14 @@
-svfdkbghfv yioeywbiyni
+@extends('layouts.app')
+
+@section('content')
+
+    @include('layouts.modalAscDelet')
+
+
+<button type="button" class="btn btn-default tut" data-dismiss="modal">тут</button>
+
+
+{!! HTML::script('/js/test.js') !!}
+
+
+@endsection
